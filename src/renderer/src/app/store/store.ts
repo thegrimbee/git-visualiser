8708b267b28d9e38091b46e701d3208a20d1509d
thegrimbee <gabriel.mario.antaputra@gmail.com>
2026-01-19 12:32:14 +0800

    Add redux integration

---
+++ src/renderer/src/app/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import appReducer from './slices/appSlice';
+import gitReducer from './slices/gitSlice';
+
+export const store = configureStore({
+  reducer: {
+    app: appReducer,
+    git: gitReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
