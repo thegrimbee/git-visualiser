68d97ae09184f76bfc01a70d50f6769f08f28a36
thegrimbee <gabriel.mario.antaputra@gmail.com>
2026-03-30 12:39:05 +0800

    Enable sandbox and context isolation

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,5 +1,4 @@
 import { contextBridge, ipcRenderer } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
 const api = {
@@ -17,7 +16,6 @@ const api = {
 // just add to the DOM global.
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
     contextBridge.exposeInMainWorld('api', api)
   } catch (error) {
     console.error(error)
